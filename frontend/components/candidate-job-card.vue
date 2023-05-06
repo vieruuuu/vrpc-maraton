7de812d046fa14a961518b5d12db5a8c90c64ad8
buzzy844 <buzzy74@yahoo.com>
2023-05-06 23:09:54 +0300

    card update comkmit

--- frontend/components/candidate-job-card.vue
+++ frontend/components/candidate-job-card.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div style="height: 400px">
+    <div style="height: 500px">
       <transition
         appear
         mode="out-in"
@@ -14,18 +14,61 @@
           />
         </div>
 
-        <q-card v-else class="full-height bg-secondary">
-          <q-card-section>
-            <div class="text-center text-h2 text-bold">Wanted</div>
-            <div>
+        <q-card v-else class="full-height bg-primary text-white">
+          <q-card-section class="bg-secondary text-primary">
+            <div class="text-center text-h3 text-bold">
+              <q-icon
+                style="transform: scaleX(-1)"
+                :name="'img:' + getAsset('revolver.png')"
+              />
+              Wanted
+              <q-icon :name="'img:' + getAsset('revolver.png')" />
+            </div>
+            <div class="text-center text-h6">
               {{ job.title }}
             </div>
+            <div class="text-center text-body1">
+              Reward:<span class="text-orange-6 text-weight-bolder">{{
+                job.reward
+              }}</span>
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <div>
+              <div class="text-body1">
+                <div class="text-bold">Description:</div>
+                {{ job.description }}
+              </div>
+            </div>
+            <div class="q-my-sm">
+              <div>
+                <q-badge
+                  v-for="badge in job.requiredBadges"
+                  :key="badge"
+                  :label="badge"
+                  outline
+                  color="blue"
+                  class="text-subtitle2 text-bold"
+                />
+              </div>
+            </div>
+            <div class="text-h6 text-bold q-mt-sm q-mb-xs">Posted by:</div>
+            <q-item class="bg-secondary">
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section class="text-primary">
+                <q-item-label>Allied Testing</q-item-label>
+                <q-item-label caption class="text-bold"> Company </q-item-label>
+              </q-item-section>
+            </q-item>
           </q-card-section>
 
-          <q-card-section> {{ job.description }} </q-card-section>
-
           <q-card-actions style="position: absolute; bottom: 0">
-            <q-btn label="Apply" />
+            <q-btn label="Apply" color="secondary" text-color="primary" />
           </q-card-actions>
         </q-card>
       </transition>
@@ -37,7 +80,7 @@
 import type { Job } from "types/job";
 
 const props = defineProps<{
-  job: Job;
+  job: any;
   flipped: boolean;
 }>();
 </script>
